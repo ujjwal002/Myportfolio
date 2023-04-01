b12363ad6275ddba6c502b90eff4785daa8851f0
Ujjwal Kumar <ujjwalsinghrajput2000@gmail.com>
2023-04-02 01:25:56 +0530

    hii

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -39,20 +39,22 @@ const Contact = () => {
       <h2 className="common-heading">Feel Free to Contact us</h2>
 
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15126.28620995241!2d73.92422475000001!3d18.59334505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c14df5c70e0d%3A0x2d19689e09e2fced!2sPhoenix%20Mall%20Washrooms!5e0!3m2!1sen!2sin!4v1658905192255!5m2!1sen!2sin"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3456.8658636574987!2d76.80990801504038!3d29.954536681915144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390e38b2e704c5ab%3A0x8dd4ee0eeffdaba4!2sHarsh%20Bhawan%20Rd%2C%20Kurukshetra%20University%2C%20Mirzapur%20Part%2C%20Haryana%20136119!5e0!3m2!1sen!2sin!4v1680378432659!5m2!1sen!2si"
         width="100%"
         height="450"
         style={{ border: 0 }}
         allowFullScreen=""
         loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"></iframe>
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
 
       <div className="container">
         <div className="contact-form">
           <form
             action="https://formspree.io/f/xgedgjkr"
             method="POST"
-            className="contact-inputs">
+            className="contact-inputs"
+          >
             <input
               type="text"
               name="username"
@@ -74,7 +76,8 @@ const Contact = () => {
               cols="30"
               rows="6"
               autoComplete="off"
-              required></textarea>
+              required
+            ></textarea>
 
             <input type="submit" value="send" />
           </form>
